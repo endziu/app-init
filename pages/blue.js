233c947a2536582a3e03b59466d823d604164eab
endziu <koper.andrzej@gmail.com>
2017-06-20 21:37:26 +0200

    typo

--- pages/blue.js
+++ pages/blue.js
@@ -6,7 +6,7 @@ export default () => (
       <div className="dtc v-mid tc white ph3 ph4-l">
         <h1 className="f4 f2-m f-subheadline-l fw6 tc">Niebieski</h1>
         <p className="center measure lh-copy bg-white-10 pa4-l pa3-m pa1">
-          jedna z addytywnych barw podstawowych, na kole barw dopełnia barwę żółtą. Zakres światła niebieskiego ma długość fali od ok. 420 do ok. 490 nm.
+          Jedna z addytywnych barw podstawowych, na kole barw dopełnia barwę żółtą. Zakres światła niebieskiego ma długość fali od ok. 420 do ok. 490 nm.
         </p>
       </div>
     </div>
