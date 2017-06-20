4e919ac7f8b1554953a1621dbce8c7d6805bb53f
endziu <koper.andrzej@gmail.com>
2017-06-20 12:47:36 +0200

    add text

--- pages/green.js
+++ pages/green.js
@@ -2,9 +2,13 @@ import Layout from '../components/layout'
 
 export default () => (
   <Layout title='Green'>
-    <div className="vh-100 dt w-100 bg-green">
-      <div className="dtc v-mid tc white ph3 ph4-l">
-        <h1 className="f6 f2-m f-subheadline-l fw6 tc">Green</h1>
+    <div className='vh-100 dt w-100 bg-green'>
+      <div className='dtc v-mid tc white ph3 ph4-l'>
+        <h1 className='f6 f2-m f-subheadline-l fw6 tc'>Green</h1>
+        <p className='center measure'>
+        Green is the color between blue and yellow on the spectrum of visible light. It is evoked by light with a predominant wavelength of roughly 495–570 nm. In the subtractive color system, used in painting and color printing, it is created by a combination of yellow and blue, or yellow and cyan; in the RGB color model, used on television and computer screens, it is one of the additive primary colors, along with red and blue, which are mixed in different combinations to create all other colors.
+        </p>
+       
       </div>
     </div>
   </Layout>
