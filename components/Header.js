fcc6bda34fea3ca88fce5072c1873dabfb7f8cdf
endziu <koper.andrzej@gmail.com>
2017-06-21 18:48:52 +0200

    border red

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Triangle from "./Triangle";
 
 export default () => (
-  <header className="fixed top-0 left-0 pa1 bg-black-80 w-100 bb bw1 b--near-white">
+  <header className="fixed top-0 left-0 pa1 bg-black-80 w-100 bb bw2 b--washed-red">
     <nav className="f4 flex items-center">
       <Link href="/">
         <a className="dim mr2 ph1">
