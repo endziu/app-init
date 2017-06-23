bd1d53ab8f9bcd1555925513817f495ac23dc359
endziu <koper.andrzej@gmail.com>
2017-06-23 13:08:40 +0200

    add link

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,12 @@
 export default () => (
-  <footer className="fixed bottom-0 bg-black white w-100 pa1">
+  <footer className="fixed bottom-0 bg-black w-100 pa1">
     <span className="flex justify-end">
-      {'#sześć-błota-stóp'}
+      <a
+        href="https://endziu.xyz"
+        className="f6 white-80 no-underline underline-hover"
+      >
+        © endziu.xyz
+      </a>
     </span>
   </footer>
 );
